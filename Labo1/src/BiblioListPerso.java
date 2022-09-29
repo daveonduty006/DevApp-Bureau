2aba3c7ff91251262f61dce87ff50e13c098fbbe
Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-09-29 16:44:14 -0400

    (Labo1) began to implement part 3 BiblioListPerso class

---
+++ Labo1/src/BiblioListPerso.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class BiblioListPerso {
+	
+	
+	
+	//
+	
+	private int tailleMax;
+	private int compteur; 
+	private ListeChainee biblio;
+	
+	//
+	
+	BiblioListPerso(List<ArrayList<String>> listeAttributs, int taille) {
+		this.compteur= 0;
+		this.tailleMax= taille;
+		chargerListe(listeAttributs);
+	}
+	
+	//
+	
+	
+	
+	//
+
+}
