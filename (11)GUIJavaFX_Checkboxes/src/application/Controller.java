31f3b1e15cd591c31d6b8d9c545ea626f9d24bab
Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-10-31 14:57:39 -0400

    (11)GUIJavaFX added but non-functionning

---
+++ (11)GUIJavaFX_Checkboxes/src/application/Controller.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+	
+	@FXML
+	private CheckBox myCheckBox;
+	@FXML
+	private Label myLabel;
+	@FXML
+	private ImageView myImageView;
+	
+	Image myImage1 = new Image(getClass().getResourceAsStream("lampOff"));
+	Image myImage2 = new Image(getClass().getResourceAsStream("lampOn"));
+	
+	public void change(ActionEvent event) {
+		if(myCheckBox.isSelected()) {
+			myLabel.setText("ON");
+			myImageView.setImage(myImage2);
+		}else {
+			myLabel.setText("OFF");
+			myImageView.setImage(myImage1);
+		}
+	}
+
+}
